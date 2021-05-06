d1e2d8529d2b3a7e75a67ea1adc468704f20e027
Gareth Tribello <gareth.tribello@gmail.com>
2021-05-06 09:18:11 +0100

    Added answer

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+xvals, yvals = np.zeros(11), np.zeros(11)
+for i in range(11) : 
+    xvals[i] = i 
+    yvals[i] = 7*i 
+
+plt.plot( xvals, yvals, 'ko' )
+plt.xlabel("Index")
+plt.ylabel("Seven times table")
+plt.savefig("seven-times-table.png")
